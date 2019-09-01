007e326dfd1fcd2d0d1dace7ea0801c122042eae
Redwan Hussain <hussain.redwan@gmail.com>
2019-09-01 20:28:59 +0600

    file added

---
+++ assets/js/plugin.js
@@ -0,0 +1,41 @@
+(function($){
+    "use strict";
+
+
+  // For Image Slider Under A Container div
+  $('.testomonial-slider').slick({
+    dots: true,
+    arrows: false,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+
+
+})(jQuery);
